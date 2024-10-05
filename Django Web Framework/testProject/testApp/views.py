323e9f5a5ab9fff66adbb8f02a23121c178cecb6
ShaunRainey <151593013+ShaunRainey@users.noreply.github.com>
2024-10-06 00:38:16 +0100

    Correct file structure so app and project are on the same tier

--- Django Web Framework/testProject/testApp/views.py
+++ Django Web Framework/testProject/testApp/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
+    #print(dir(request))
+    print(request)
+    #print(request.content_params)
     return HttpResponse('Welcome to Little Lemon Restaurant')
 # Create your views here.   
