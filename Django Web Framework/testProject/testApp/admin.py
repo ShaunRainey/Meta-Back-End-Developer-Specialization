51b9d9ae1e5e924a87c30b6113f7baeddaf75f83
ShaunRainey <151593013+ShaunRainey@users.noreply.github.com>
2024-10-08 23:45:54 +0100

    Add a superuser to test admin features

--- Django Web Framework/testProject/testApp/admin.py
+++ Django Web Framework/testProject/testApp/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(Menu)
-admin.site.register(MenuCategory)
+#admin.site.register(Menu)
+#admin.site.register(MenuCategory)
+
+admin.site.register(Reservations)
--- Django Web Framework/testProject/testApp/models.py
+++ Django Web Framework/testProject/testApp/models.py
@@ -16,3 +16,13 @@ from django.db import models
 #    menu_item = models.CharField(max_length = 200, default = None)
 #    price = models.IntegerField(null = False)
 #    category_id = models.ForeignKey(MenuCategory, on_delete = models.PROTECT, default = None, related_name = "category_name")  
+
+class Reservations(models.Model):
+    name = models.CharField(max_length = 100, blank = True)
+    contact = models.CharField('Phone number',max_length=300)
+    time = models.TimeField()
+    count = models.IntegerField()
+    notes = models.CharField(max_length=300,blank=True)
+
+    def __str__(self):
+        return self.name
