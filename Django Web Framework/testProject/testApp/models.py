1cbb4f6d54b730c901781169ff6c8b4877549322
ShaunRainey <151593013+ShaunRainey@users.noreply.github.com>
2024-10-08 23:28:51 +0100

    Reset models.py

--- Django Web Framework/testProject/testApp/models.py
+++ Django Web Framework/testProject/testApp/models.py
@@ -9,10 +9,10 @@ from django.db import models
 
 ### Creating a one-to-many relation
 
-class MenuCategory(models.Model):
-    menu_category_name = models.CharField(max_length = 200)
+#class MenuCategory(models.Model):
+#    menu_category_name = models.CharField(max_length = 200)
 
-class Menu(models.Model):
-    menu_item = models.CharField(max_length = 200, default = None)
-    price = models.IntegerField(null = False)
-    category_id = models.ForeignKey(MenuCategory, on_delete = models.PROTECT, default = None, related_name = "category_name")  
+#class Menu(models.Model):
+#    menu_item = models.CharField(max_length = 200, default = None)
+#    price = models.IntegerField(null = False)
+#    category_id = models.ForeignKey(MenuCategory, on_delete = models.PROTECT, default = None, related_name = "category_name")  
